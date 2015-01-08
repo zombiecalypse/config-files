58fa31974abd65b3a7d80b134e80a431d27475bb
Aaron Karper <akarper@google.com>
2015-01-08 22:31:07 +0100

    Add more installation into setup.rb

--- setup.rb
+++ setup.rb
@@ -13,6 +13,9 @@ home.join('.bin', 'conf').open("w") do |f|
 end
 home.join('.bin', 'conf').chmod(0755)
 
+FileUtils.ln_sf(confpath.join('bin', 'vim-html-markdown'), home.join('.bin'))
+home.join('.bin', 'vim-html-markdown').chmod(0755)
+
 csv = open(confpath.join('config-files.list')) {|f| CSV.parse(f, headers: true)}
 original, inrel = csv.headers
 
@@ -24,3 +27,14 @@ csv.each do |row|
   end
   FileUtils.ln_sf(in_path, orig)
 end
+
+installed = confpath.join('installed').open.readlines.collect {|e| e.strip}
+system "sudo apt-get install -y #{installed.join(' ')}"
+
+system "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
+system "\\curl -sSL https://get.rvm.io | bash -s stable"
+
+system "git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh"
+system "chsh -s /bin/zsh"
+
+system "sudo pip install ropevim"
